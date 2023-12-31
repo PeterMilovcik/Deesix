d5760997db5ecc9a7215c0882d537b857efd0e0b
Peter Milovcik <peter.milovcik@gmail.com>
2023-12-31 13:39:16 +0100

    Add Biome

---
+++ Deesix.Domain/Entities/Biome.cs
@@ -0,0 +1,31 @@
+﻿namespace Deesix.Domain.Entities;
+
+public class Biome
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "Unknown";
+    public string Description { get; set; } = "Unknown";    
+    public string Climate { get; set; } = "Unknown";
+
+    public override string ToString() => Name;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Biome biome)
+        {
+            return Id == biome.Id &&
+                   Name == biome.Name &&
+                   Description == biome.Description &&
+                   Climate == biome.Climate;
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Id, Name, Description, Climate);
+
+    public static bool operator ==(Biome? left, Biome? right) => EqualityComparer<Biome>.Default.Equals(left, right);
+    public static bool operator !=(Biome? left, Biome? right) => !(left == right);
+
+    public static Biome Unknown => new Biome();
+}
---
+++ Deesix.Test/Domain/Entities/BiomeTests.cs
@@ -0,0 +1,164 @@
+﻿using Deesix.Domain.Entities;
+
+namespace Deesix.Test.Domain.Entities;
+
+[TestFixture]
+public class BiomeTests
+{
+    [Test]
+    public void Biome_Defaults()
+    {
+        // Arrange
+        var biome = new Biome();
+
+        // Assert
+        Assert.That(biome.Id, Is.EqualTo(0));
+        Assert.That(biome.Name, Is.EqualTo("Unknown"));
+        Assert.That(biome.Description, Is.EqualTo("Unknown"));
+        Assert.That(biome.Climate, Is.EqualTo("Unknown"));
+    }
+
+    [Test]
+    public void Biome_CustomValues()
+    {
+        // Arrange
+        var biome = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        // Assert
+        Assert.That(biome.Id, Is.EqualTo(1));
+        Assert.That(biome.Name, Is.EqualTo("Forest"));
+        Assert.That(biome.Description, Is.EqualTo("A dense forest biome"));
+        Assert.That(biome.Climate, Is.EqualTo("Temperate"));
+    }
+
+    [Test]
+    public void Biome_ToString()
+    {
+        // Arrange
+        var biome = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        // Assert
+        Assert.That(biome.ToString(), Is.EqualTo("Forest"));
+    }
+
+    [Test]
+    public void Biome_Equals()
+    {
+        // Arrange
+        var biome1 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        var biome2 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        // Assert
+        Assert.That(biome1, Is.EqualTo(biome2));
+    }
+
+    [Test]
+    public void Biome_GetHashCode()
+    {
+        // Arrange
+        var biome1 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        var biome2 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        // Assert
+        Assert.That(biome1.GetHashCode(), Is.EqualTo(biome2.GetHashCode()));
+    }
+
+    [Test]
+    public void Biome_EqualsOperator()
+    {
+        // Arrange
+        var biome1 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        var biome2 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        // Assert
+        Assert.That(biome1 == biome2, Is.True);
+    }
+
+    [Test]
+    public void Biome_NotEqualsOperator()
+    {
+        // Arrange
+        var biome1 = new Biome
+        {
+            Id = 1,
+            Name = "Forest",
+            Description = "A dense forest biome",
+            Climate = "Temperate"
+        };
+
+        var biome2 = new Biome
+        {
+            Id = 2,
+            Name = "Desert",
+            Description = "A dry desert biome",
+            Climate = "Arid"
+        };
+
+        // Assert
+        Assert.That(biome1 != biome2, Is.True);
+    }
+
+    [Test]
+    public void Biome_Unknown()
+    {
+        // Arrange
+        var biome = Biome.Unknown;
+
+        // Assert
+        Assert.That(biome.Id, Is.EqualTo(0));
+        Assert.That(biome.Name, Is.EqualTo("Unknown"));
+        Assert.That(biome.Description, Is.EqualTo("Unknown"));
+        Assert.That(biome.Climate, Is.EqualTo("Unknown"));
+    }
+}
